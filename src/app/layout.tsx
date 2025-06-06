e83a451eaf09b594670f99a4e91eaa59f514b90a
Tanner Bearden <tannerbeardens@gmail.com>
2025-06-06 16:00:02 -0600

    analy

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { ReactNode } from "react";
 import Footer from "../components/Footer";
 import BackgroundCanvas from "../components/BackgroundCanvas";
+import { Analytics } from "@vercel/analytics/next"; // <-- import this
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
@@ -17,6 +18,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
         <BackgroundCanvas />
         <main>{children}</main>
         <Footer />
+        <Analytics /> {/* <-- add this line */}
       </body>
     </html>
   );
